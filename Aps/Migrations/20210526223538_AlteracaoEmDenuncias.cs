9c4afba8350159b105cf9f72a411b5323e8e84da
Doris Luvizute <re041650@resource.com.br>
2021-05-26 19:39:45 -0300

    Atualização do Banco de Dados

---
+++ Aps/Migrations/20210526223538_AlteracaoEmDenuncias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aps.Migrations
+{
+    public partial class AlteracaoEmDenuncias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContinenteId",
+                table: "Denuncias",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContinenteId",
+                table: "Denuncias");
+        }
+    }
+}
